888d100f1a5d10d0bda38aa194f27afb9625514b
Allecan <allecan52@gmail.com>
2022-09-16 15:53:25 -0600

    responsive changes
    las secciones de que es, para quienes y por quienes ya se visualizan
    bien en desktop y mobile

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -41,8 +41,8 @@ const LandingPage = () => {
           />
         </div>
         {/* contenido en mobile */}
-        <div className="text-center md:hidden">
-          <h1 className="mb-5 md:hidden">Hola, ¿Qué tal?</h1>
+        <div className="text-center flex flex-col items-center gap-5 md:hidden ">
+          <h1 className="m-0 md:hidden">Hola, ¿Qué tal?</h1>
           <p className="md:hidden">
             Bienvenido docente a Atenea, su nueva herramienta de apoyo académico
           </p>
@@ -61,6 +61,17 @@ const LandingPage = () => {
           >
             Registrarse
           </button>
+          <a
+            href="#aboutInit"
+            className="btntxtsmmobl text-[#4D3483] flex flex-col items-center mb-5"
+          >
+            Acerca de Atenea{" "}
+            <img
+              className="mx-auto mt-1"
+              src={ChevronDownIcon}
+              alt="ChevronDownIcon"
+            />
+          </a>
         </div>
 
         {/* contenido en desktop */}
@@ -74,17 +85,6 @@ const LandingPage = () => {
               puedas utilizar de forma completa nuestra plataforma web académica
             </p>
 
-            {/* <button
-              className="arrow-animation btntxtsmdesk text-[#4D3483] hover:text-[#FF54B0] w-40 h-11"
-              onClick={handleAbout}
-            >
-              Acerca de Atenea
-              <img
-                className="mx-auto mt-2"
-                src={ChevronDownIcon}
-                alt="ChevronDownIcon"
-              />
-            </button> */}
             <a
               href="#aboutInit"
               className="arrow-animation btntxtsmdesk text-[#4D3483] hover:text-[#FF54B0] w-40 h-11"
@@ -98,15 +98,19 @@ const LandingPage = () => {
             </a>
           </div>
         </div>
-
-        {/* contenido del about */}
-        <div className="mb-20 mr-8" id="aboutInit">
-          <div className="mx-[9%] my-[30%]">
-            <div className="mb-12 flex flex-row items-center gap-4">
-              <h1 className="bigtitdesk m-auto">¿Qué es Atenea?</h1>
+      </div>
+      {/* contenido del about */}
+      {/* what is space */}
+      <div className="mx-5 flex flex-col justify-between md:mx-16 md:grid md:grid-cols-2 mb-8 md:mb-20">
+        <div className="md:mb-20 md:mr-8" id="aboutInit">
+          <div className="mb-6 md:mx-[9%] md:my-[30%]">
+            <div className="mb-6 md:mb-12 flex flex-row items-center gap-4">
+              <h1 className="m-auto text-[1.777rem] md:text-[3.157rem]">
+                ¿Qué es Atenea?
+              </h1>
               <div className="bg-[#DBD8FF] h-[4px] flex-none order-1 grow rounded-sm"></div>
             </div>
-            <p className="normtxtdesk text-justify">
+            <p className="hidden text-[1.333rem] text-justify md:block">
               Atenea es un proyecto social pensado y desarrollado para apoyar y
               facilitar las actividades cotidianas que se realizan dentro de un
               centro educativo.
@@ -118,34 +122,96 @@ const LandingPage = () => {
             </p>
           </div>
         </div>
-        <div className="bg-[#FCFBFF] bordeblur  md:rounded-[20px] md:mb-20">
-          <img
-            className="hidden mx-auto md:flex"
-            src={WhatIsImg}
-            alt="firstimage"
-          />
+        <div className="bg-[#FCFBFF] bordeblurd mb-6 md:mb-0">
+          <img className="mx-auto md:flex" src={WhatIsImg} alt="WhatIsImg" />
         </div>
-
-        {/* <div className="items-center">
-          <div className="grid grid-cols-3">
-            <div className="bg-[#DBD8FF]  h-[4px] w-[720px] mt-[115px] ml-[70px]"></div>
-            <h1 className="text-center font-bold text-[51px] my-[80px]">
-              El Equipo
-            </h1>
-            <div className="bg-[#DBD8FF] h-[4px] w-[720px] mt-[116px] ml-[-175px]"></div>
+        <p className="text-center md:hidden">
+          Atenea es un proyecto social pensado y desarrollado para apoyar y
+          facilitar las actividades cotidianas que se realizan dentro de un
+          centro educativo.
+          <br /> <br />
+          Es una plataforma web cuyo fin es lograr la automatización en las
+          tareas realizadas por las personas que utilizan Atenea para que las
+          mismas puedan tener un mejor control de toda la información educativa
+          que manejen.
+        </p>
+      </div>
+      {/* for whom space */}
+      <div className="mx-5 flex flex-col justify-between md:mx-16 md:grid md:grid-cols-2 mb-8 md:mb-20">
+        <div className="md:order-2 md:h-[880px]">
+          <div className="mb-6 md:mx-[9%] md:my-[30%]">
+            <div className="mb-6 md:mb-12 flex flex-row items-center gap-4">
+              <h1 className="m-auto text-[1.777rem] md:text-[3.157rem] order-2">
+                ¿Para quiénes?
+              </h1>
+              <div className="bg-[#DBD8FF] h-[4px] flex-none order-1 grow rounded-sm"></div>
+            </div>
+            <p className="hidden text-[1.333rem] text-justify md:block">
+              Atenea nació gracias a la colaboración entre Equipo Xapps y las
+              personas de la Escuela Oficial Rural Mixta cantón el Llano, aldea
+              Ocubilá, en Huehuetenango.
+              <br /> <br />
+              Por ello, Atenea fue concebida para apoyarles y solucionar
+              problemas puntuales que presentaban al momento de llevar a cabo
+              tareas educativas comunes como la toma y registro de notas y
+              asistencias, la consulta de los boletines y el control en general
+              de la información de los estudiantes y docentes que conforman la
+              escuela.
+            </p>
           </div>
-
-          <div className="grid grid-cols-4 items-center gap-[40px]">
-          <AboutProfile ruta={Profile} nombre = {"Kelvin Cano"} rol = {"Scrum Master & UI / UX Designer"} />
-          <AboutProfile ruta={Profile} nombre = {"Josué Méndez"} rol = {"Product Owner & Backend Developer"} /> 
-          <AboutProfile ruta={Profile} nombre = {"Fernando Juárez"} rol = {"Frontend Manager"} /> 
-          <AboutProfile ruta={Profile} nombre = {"Daniel Tistoj"} rol = {"Backend Manager"} />  
-          <AboutProfile ruta={Profile} nombre = {"Luis Rodríguez"} rol = {"Frontend Developer"} /> 
-          <AboutProfile ruta={Profile} nombre = {"Manuel Villagrán"} rol = {"Frontend Developer"} /> 
-          <AboutProfile ruta={Profile} nombre = {"Fredy Mateo"} rol = {"Frontend Developer"} /> 
-          <AboutProfile ruta={Profile} nombre = {"Jonathan Martínez"} rol = {"Backend Developer"} /> 
+        </div>
+        <div className="bg-[#FCFBFF] bordeblurd mb-6 md:mb-0 md:h-[880px] md:order-1">
+          <img className="mx-auto md:flex" src={ForWhomImg} alt="ForWhomImg" />
+        </div>
+        <p className="text-center md:hidden">
+          Atenea nació gracias a la colaboración entre Equipo Xapps y las
+          personas de la Escuela Oficial Rural Mixta cantón el Llano, aldea
+          Ocubilá, en Huehuetenango.
+          <br /> <br />
+          Por ello, Atenea fue concebida para apoyarles y solucionar problemas
+          puntuales que presentaban al momento de llevar a cabo tareas
+          educativas comunes como la toma y registro de notas y asistencias, la
+          consulta de los boletines y el control en general de la información de
+          los estudiantes y docentes que conforman la escuela.
+        </p>
+      </div>
+      {/* by whom space */}
+      <div className="mx-5 flex flex-col justify-between md:mx-16 md:grid md:grid-cols-2 mb-8 md:mb-20">
+        <div className="md:h-[880px]">
+          <div className="mb-6 md:mx-[9%] md:my-[30%]">
+            <div className="mb-6 md:mb-12 flex flex-row items-center gap-4">
+              <h1 className="m-auto text-[1.777rem] md:text-[3.157rem]">
+                ¿Por quiénes?
+              </h1>
+              <div className="bg-[#DBD8FF] h-[4px] flex-none order-1 grow rounded-sm"></div>
+            </div>
+            <p className="hidden text-[1.333rem] text-justify md:block">
+              Atenea fue desarrollado por Equipo Xapps, un equipo conformado por
+              ocho estudiantes, de 5to año en Ingeniería en Informática y
+              Sistemas, de la Universidad Rafel Landívar, Campus San Alberto
+              Hurtado S. J., de Quetzaltenango.
+              <br /> <br />
+              Para todos nosotros fue una experiencia importante y gratificante
+              el poder haber tenido la oportunidad de colaborar en el desarrollo
+              de este proyecto con las personas de la Escuela a lo largo de 5
+              meses, los cuales dieron fruto a esta plataforma web.
+            </p>
           </div>
-        </div> */}
+        </div>
+        <div className="bg-[#FCFBFF] bordeblurd mb-6 md:mb-0 md:h-[880px]">
+          <img className="mx-auto md:flex" src={ForWhomImg} alt="ByWhomImg" />
+        </div>
+        <p className="text-center md:hidden">
+          Atenea fue desarrollado por Equipo Xapps, un equipo conformado por
+          ocho estudiantes, de 5to año en Ingeniería en Informática y Sistemas,
+          de la Universidad Rafel Landívar, Campus San Alberto Hurtado S. J., de
+          Quetzaltenango.
+          <br /> <br />
+          Para todos nosotros fue una experiencia importante y gratificante el
+          poder haber tenido la oportunidad de colaborar en el desarrollo de
+          este proyecto con las personas de la Escuela a lo largo de 5 meses,
+          los cuales dieron fruto a esta plataforma web.
+        </p>
       </div>
     </>
   );
