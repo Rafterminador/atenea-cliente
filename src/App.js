a2aab5065085126b1bee862a116fc31ca98ed713
Allecan <allecan52@gmail.com>
2022-09-17 09:25:46 -0600

    About.jsx deleted
    Se elimino la pagina anterior del about y se quito la referencia de ruta
    a esta pagina en el app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { Login } from "./pages/login";
 import { RegistroUsuario } from "./pages/RegistroUsuario";
-import { About } from "./pages/About";
 import OlvidePassword from "./pages/OlvidePassword";
 import Check from "./pages/Check";
 import RestorePassword from "./pages/RestorePassword";
@@ -14,8 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/registro/usuario" element={<RegistroUsuario />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/registro/usuario" element={<RegistroUsuario />} />      
         <Route path="/olvide-password" element={<OlvidePassword />} />
         <Route path="/verificacion" element={<Check />} />
         <Route path="/restablecer-password" element={<RestorePassword />} />
