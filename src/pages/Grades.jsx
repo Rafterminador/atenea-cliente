f303c920781e26c7c56ef4827a985282c97e45b2
fernando <ferrafterminador@gmail.com>
2022-10-30 10:15:26 -0600

    obteniendo grados de firebase

--- src/pages/Grades.jsx
+++ src/pages/Grades.jsx
@@ -25,27 +25,19 @@ const Grades = () => {
 
     getAllGradesBackend();
   }, []);
-  const getGrades = grades.map((grade) =>
-    <div key={grade.id}><Grade curso={grade.grade_name} encargado={grade.teacherRef.displayName} id={grade.id} alumnos={0} /></div>
+  const getGrades = grades.map((levelGrade) =>
+    levelGrade.grades.map((grade) =>
+      <>
+        {grade.position === 0 ? <h1 className='h1-administracion' key={levelGrade.id}>{levelGrade.level_name}</h1> : ""}
+        <Grade key={grade.id} curso={grade.grade_name} encargado={grade.teacherRef.displayName} id={grade.id} alumnos={grade.totalStudents} />
+      </>
+    )
   )
   return (
     <>
       <div className='contenedor contenedor-admin mb-[80px]'>
         <SearchBar />
-        <h1 className='h1-administracion'>Nivel Pre-primario</h1>
         {getGrades}
-        {/* <Grade curso={"Pre-kínder"} encargado={"Rubén	Argüero	Sánchez Juanajatoja"} id={"1"} alumnos={0} />
-        <Grade curso={"Kínder"} encargado={"Anastasia Madeline Orellana Álvarez"} id={"2"} alumnos={0} />
-        <Grade curso={"Párvulos"} encargado={"Sara Josthyn Gutiérrez Lagos"} id={"3"} alumnos={0} /> */}
-        {/* <h1 className='h1-administracion'>Nivel Primario</h1> */}
-        {/* <Grade curso={"Primero primaria"} encargado={"Rubén	Argüero	Sánchez Juanajatoja"} id={"4"} alumnos={0} />
-        <Grade curso={"Segundo primaria"} encargado={"Anastasia Madeline Orellana Álvarez"} id={"5"} alumnos={0} />
-        <Grade curso={"Tercero primaria"} encargado={"Sara Josthyn Gutiérrez Lagos"} id={"6"} alumnos={0} />
-        <Grade curso={"Cuarto primaria"} encargado={"Nadie"} id={"7"} alumnos={0} />
-        <Grade curso={"Quinto primaria"} encargado={"Nadie"} id={"8"} alumnos={0} />
-        <Grade curso={"Sexto primaria"} encargado={"Cain Nehemias Orellana Valenzuela"} id={"9"} alumnos={0} /> */}
-        {/* <h1 className='h1-administracion'>Otros</h1> */}
-        {/* <Grade curso={"Educación física"} encargado={"Yonathan Fabio Aguilera Poblete"} id={"10"} alumnos={0} /> */}
         <AddButton function={handleClick} />
       </div>
       <BottomNavbar />
