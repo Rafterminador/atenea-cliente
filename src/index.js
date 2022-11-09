6622f8029f84f4b49ce93839ed688339098505b4
Allecan <allecan52@gmail.com>
2022-11-09 11:15:53 -0600

    Customized pwa v1
    Changed from using the default react pwa configuration to using workbox via cdn.
    Currently it stays offline in the necessary paths and caches the POST request of the /assistance path.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./css/components.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
---
+++ src/sw-template.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+const API_URL = "https://atenea-app-ud23b.ondigitalocean.app/";
+workbox.loadModule("workbox-background-sync");
+const { precacheAndRoute, createHandlerBoundToURL } = workbox.precaching;
+const { registerRoute, NavigationRoute } = workbox.routing;
+const { NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+if (!location.origin.includes("localhost")) {
+  workbox.setConfig({ debug: false });
+}
+
+// precache all assets, images, txt, js, css and index.html
+precacheAndRoute(self.__WB_MANIFEST);
+
+//register allow offline navigation routes
+const handler = createHandlerBoundToURL("/index.html");
+const navigationRoute = new NavigationRoute(handler, {
+  allowlist: [new RegExp("/home/docente"), new RegExp("/asistencia/*")],
+});
+registerRoute(navigationRoute);
+
+// // Gets Offline
+// const cacheNetworkFirst = [
+//   "/api/v1/student/getall-students/",
+//   "/api/v1/user/get-users/",
+// ];
+
+// registerRoute(({ request, url }) => {
+//   // console.log({request, url})
+//   if (cacheNetworkFirst.includes(url.pathname)) return true;
+
+//   return false;
+// }, new NetworkFirst());
+
+// Posts Offline
+const bgSyncPlugin = new BackgroundSyncPlugin("offline-posts", {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp(API_URL + "api/v1/attendence/new-attendence"),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  "POST"
+);
